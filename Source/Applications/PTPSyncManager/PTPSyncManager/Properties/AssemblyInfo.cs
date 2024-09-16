5a72a8028c3e1d2cb36b914c7754856e61042c6e
gsfbuildbot <support@gridprotectionalliance.org>
2024-09-16 02:30:45 -0400

    PTPSync: Version change for build v1.1.262.0-master.

--- Source/Applications/PTPSyncManager/PTPSyncManager/Properties/AssemblyInfo.cs
+++ Source/Applications/PTPSyncManager/PTPSyncManager/Properties/AssemblyInfo.cs
@@ -53,6 +53,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.261.0")]
-[assembly: AssemblyVersion("1.1.261.0")]
-[assembly: AssemblyFileVersion("1.1.261.0")]
+// [assembly: AssemblyVersion("1.1.262.0")]
+[assembly: AssemblyVersion("1.1.262.0")]
+[assembly: AssemblyFileVersion("1.1.262.0")]

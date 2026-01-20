e1b3ac25dc83d857d6d881949a3e79b5e1d064b0
gsfbuildbot <support@gridprotectionalliance.org>
2026-01-20 03:06:13 -0500

    PTPSync: Version change for build v1.1.363.0-master.

--- Source/Applications/PTPSync/PTPSyncServices/Properties/AssemblyInfo.cs
+++ Source/Applications/PTPSync/PTPSyncServices/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.362.0")]
+[assembly: AssemblyVersion("1.1.363.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
